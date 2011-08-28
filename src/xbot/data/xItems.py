ddb0e563c66812d8c86b94c484b9d8ba45fad4c8
Taylor Kozak <anachronistictaylor@gmail.com>
2011-08-28 01:52:43 -0400

    I hope this one works.

---
+++ src/xbot/data/xItems.py
@@ -0,0 +1,14 @@
+#Items required by xbot.
+#Left in arrary format for easier expansion in the future.
+items = [
+    {
+        "id" : 571,
+        "descId" : 851122901,
+        "name" : "Jumbo Dr. Lucifer",
+        "type" : "beverage",
+        "image" : "drlucifer.gif",
+        "autosell" : 50,
+        "fullness" : 1,
+        "adventuresGained" : "1-3",
+    },
+]
